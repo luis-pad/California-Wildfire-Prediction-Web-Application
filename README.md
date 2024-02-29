9c730d430abbfab18d1a170e521369ee6f049e85
Luis <luis.padilla.429@my.csun.edu>
2024-02-28 19:54:34 -0800

    Readded all code from previous repository

---
+++ README.md
@@ -0,0 +1,48 @@
+# California Wildfire Prediction
+![Alt text](CSUN-senior-design-project.png)
+
+> A machine learning project for predicting wildfires in California using data-driven approaches.
+
+## Table of Contents
+
+- [Description](#description)
+- [Key Features](#key-features)
+- [Team Members](#team-members)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Data Sources](#data-sources)
+- [Project Structure](#project-structure)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Description
+
+The California Wildfire Prediction project is aimed at mitigating the increasing wildfire threat in California by leveraging data-driven approaches. This repository contains the code and resources for building predictive models, processing data, and visualizing results. The project employs machine learning techniques, including deep learning, to forecast wildfire risks based on historical data.
+
+## Key Features
+
+- data collection from various sources.
+- Data preprocessing, including cleaning and feature engineering.
+- Machine learning models for wildfire risk prediction.
+- Interactive visualizations and user-friendly dashboards.
+- Continuous model evaluation and improvement.
+
+## Team Members
+
+Meet the team members who have contributed to this project:
+
+- [Farid Koushaneh](https://github.com/faridko26)
+- [Alondra Sanchez](https://github.com/Whimss)
+- [Luis Padilla](https://github.com/luis-pad)
+- [Matthew Bellman]()
+- [Edwin Rivera]()
+
+## Installation
+
+To set up the project on your local machine, follow these steps:
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/faridko26/California-Wildfire-Prediction.git
+
---
+++ app.py
@@ -0,0 +1,205 @@
+from flask import Flask, render_template, redirect, url_for, request, send_file, flash
+from pymongo.mongo_client import MongoClient
+from io import BytesIO
+from datetime import date
+
+# LIST OF STRINGS FILLED WITH CALIFORNA COUNTY NAMES
+# Format: <BLANK>,<COUNTY NAME>,...
+CountyList = ["","Alameda",
+              "","Alpine",
+              "","Amador",
+              "","Butte",
+              "","Calaveras",
+              "","Colusa",
+              "","Contra Costa",
+              "","Del Norte",
+              "","El Dorado",
+              "","Fersno",
+              "","Glenn",
+              "","Humboldt",
+              "","Imperial",
+              "","Inyo",
+              "","Kern",
+              "","Kings",
+              "","Lake",
+              "","Lassen",
+              "","Los Angeles",
+              "","Madera",
+              "","Marin",
+              "","Mariposa",
+              "","Mendocino",
+              "","Merced",
+              "","Modoc",
+              "","Mono",
+              "","Monterey",
+              "","Napa",
+              "","Nevada",
+              "","Orange",
+              "","Placer",
+              "","Plumas",
+              "","Riverside",
+              "","Sacramento",
+              "","San Benito",
+              "","San Bernardino",
+              "","San Diego",
+              "","San Francisco",
+              "","San Joaquin",
+              "","San Luis Obispo",
+              "","San Mateo",
+              "","Santa Barbara",
+              "","Santa Clara",
+              "","Santa Cruz",
+              "","Shasta",
+              "","Sierra",
+              "","Siskiyou",
+              "","Solano",
+              "","Sonama",
+              "","Stanislaus",
+              "","Sutter",
+              "","Tehama",
+              "","Trinity",
+              "","Tulare",
+              "","Tuolume",
+              "","Ventura",
+              "","Yolo",
+              "","Yuba"
+              ]
+
+## Generates the file to download ##
+def createFile(data):
+    file_data = bytearray('\n'.join(data) , "utf-8")
+    return send_file(BytesIO(file_data), download_name = "dataset.txt", as_attachment = True)
+
+## Finds and returns a value from file 'filePath' from day 'date' and county 'county' ##
+## Returns an empty array if it can't find a value ##
+def findValue(start_date, end_date, county, filePath):
+    file = open(filePath, 'r')
+    line = file.readline()
+    line = file.readline() # Skip Header
+    matches = []
+
+    if (end_date == ""): # Only Start Date
+        end_date = start_date
+
+    while (line != ''):
+        line_check = [str(x) for x in line.split(",")]
+        if ((start_date <= date.fromisoformat(toISO(line_check[0])) <= end_date)
+        and (county in line)):
+            matches.append(line_check)
+        line = file.readline()
+
+    file.close()
+
+    if matches == []:
+        return []
+
+    for i in matches:
+        i[0] = date.fromisoformat(toISO(i[0]))
+        
+    ret_list = []
+    for i in matches:
+        # <County>  |  <Date>  |  <Value>
+        ap_str = i[3][:-1] + "  |  " + str(i[0]) + "  |  " + i[1]
+        ret_list.append(ap_str)
+
+    ret_list.sort()
+    ret_list.append("")
+    
+    return ret_list
+
+## Converts the county id into a string ##
+def formatCounty(county):
+    try:
+        return "," + f"{county:03d}"+ "," + CountyList[county] + "\n"
+    except:         # Out of range index
+        return ""
+        
+## Converts the date from mm/dd/yyyy to yyyy-mm-dd##
+def toISO(date):
+    m, d, y = [str(x) for x in date.split('/')]
+    return y + "-" + m + "-" + d
+
+app = Flask(__name__)
+app.secret_key = b'z19D]p3Q4]rjhx[qcBHSf-Rx@K@9W*' # Needed for flash()
+
+client = MongoClient('localhost', 27017)
+db = client["cwp_prod"]
+coll = db.main
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/data')
+def data():
+    sd = request.args.get('start_date', default = "", type = str)
+    ed = request.args.get('end_date', default = "", type = str)
+    county = request.args.getlist('county', type = int)
+    print(sd)       # DEBUG
+    print(ed)       # DEBUG
+    print(county)   # DEBUG
+
+    if (len(county) == 0 or sd == ""): # NO DATA ENTERED
+        return render_template('data.html')
+    
+    try:
+        start_date = date.fromisoformat(sd)
+        end_date = date.fromisoformat(ed) if (not ed == "") else ""
+    except: ## INVALID DATE ENTRY
+        print("Invalid date entry!")
+        flash("ERROR: Invalid date entry!")
+        return redirect(url_for('data'))
+
+    if (start_date > end_date):
+        print("End date is larger than start date")
+        flash("ERROR: End date is larger than start date")
+        return redirect(url_for('data'))
+
+    print(start_date)     # DEBUG date : <year>-<month>-<day>
+    print(end_date)
+    
+    value_NDVI = []
+    value_NDVI.append(["<County>  |  <Date>  |  <NDVI>", ""])
+        
+    for i in county:
+        find_county = formatCounty(i)
+        print(find_county)                  # DEBUG
+
+        value_NDVI.append(
+            findValue(start_date, end_date, find_county, "Data Processing/output/NDVI_result.csv")
+        )
+
+    value_NDVI = sum(value_NDVI, []) # Flatens the list
+    
+    for i in value_NDVI: # DEBUG, OUTPUT
+        print(i)
+    
+    return createFile(value_NDVI)
+
+@app.route('/models')
+def models():
+    return render_template('models.html')
+
+@app.route('/data_sources')
+def data_sources():
+    return render_template('data_sources.html')
+
+@app.route('/dbtest')
+def db_test():
+
+    year_param = request.args.get('year')
+
+    # 1
+    _1 = coll.find({"year": int(year_param)}, {"_id": 0})
+    return(list(_1))
+
+if __name__ == "__main__":
+    app.run()
